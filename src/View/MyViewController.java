5212ff01042c4095610990b7d0be56f755448506
amitwitk <104445059+amitwitk@users.noreply.github.com>
2022-06-19 14:45:12 +0300

    AFTERjar

--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,4 +1,11 @@
 package View;
-
+import algorithms.mazeGenerators.Maze;
 public class MyViewController implements IView{
+    public void start_click(){
+        System.out.println("or the queen");
+    }
+    public void orSaysClick(){
+        System.out.println("amit the king");
+    }
+
 }
