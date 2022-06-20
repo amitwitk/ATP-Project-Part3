1d5a905b0069abb60e0fd3d6b1608946f6658943
or <or.fuchs12@gmail.com>
2022-06-20 11:59:47 +0300

    a little

--- src/View/MazeDisplay.java
+++ src/View/MazeDisplay.java
@@ -3,6 +3,9 @@ package View;
 import java.awt.*;
 import algorithms.mazeGenerators.Maze;
 import algorithms.search.Solution;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
 
 
 public class MazeDisplay extends Canvas {
@@ -12,9 +15,44 @@ public class MazeDisplay extends Canvas {
     int col_index;
 
     public MazeDisplay(GraphicsConfiguration config, int row_index, int col_index) {
-        super(config);
+        //super(config);
         this.row_index = row_index;
         this.col_index = col_index;
 
     }
+
+    public void drawMaze(Maze maze) {
+        this.maze = maze;
+        draw();
+    }
+
+    private void draw() {
+        if(maze != null){
+            double canvasHeight = getHeight();
+            double canvasWidth = getWidth();
+            int rows = maze.getRows();
+            int cols = maze.getColumns();
+
+            double cellHeight = canvasHeight / rows;
+            double cellWidth = canvasWidth / cols;
+
+            GraphicsContext graphicsContext = getGraphicsContext2D();
+            //clear the canvas:
+            graphicsContext.clearRect(0, 0, canvasWidth, canvasHeight);
+            graphicsContext.setFill(Color.RED);
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    if(maze.getValue(j,i) == 1){
+                        //if it is a wall:
+                        double x = j * cellWidth;
+                        double y = i * cellHeight;
+                        graphicsContext.fillRect(x, y, cellWidth, cellHeight);
+                    }
+                }
+            }
+        }
+    }
+
+
 }
--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,5 +1,6 @@
 package View;
-import algorithms.mazeGenerators.Maze;
+
+
 public class MyViewController implements IView{
     public void start_click(){
         System.out.println("or the queen");
@@ -8,6 +9,13 @@ public class MyViewController implements IView{
         System.out.println("amit the king");
         System.out.println("works?");
     }
+
+    public void generateButtonMaze () {}
+
+
+
+
+
 
 
 
